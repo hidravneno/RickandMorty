ac4e5e2a7e1505152809fb8b6eac5d55ea8a42a4
aramburo reyes francisco eduardo <aramburoeduardo972@gmail.com>
2025-10-23 17:24:39 -0700

    Initial Commit

---
+++ RickandMorty/App/RickandMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickandMortyApp.swift
+//  RickandMorty
+//
+//  Created by francisco eduardo aramburo reyes on 23/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RickandMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
